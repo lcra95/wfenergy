50016320cd6b7540db5cf602669685eb229b4b60
Elianny <admin@lrsystems.cl>
2018-11-08 23:58:18 -0300

    Prueba de Recepcion de Dtes

---
+++ USERS/API/leefactura.php
@@ -0,0 +1,101 @@
+<?php
+include_once ("conexion.php");
+
+
+class leeXml{
+
+
+    function getDtes($xml){
+
+        $doc=new DOMDocument();
+        $doc->load($xml);
+        $encabezados=$doc->getElementsByTagName('IdDoc');
+		foreach ($encabezados as $encabezado) 
+		{
+		//Acceso al TAG
+            $TipoDTE=$encabezado->getElementsByTagName('TipoDTE');
+            $folio=$encabezado->getElementsByTagName('Folio');
+			$fecha=$encabezado->getElementsByTagName('FchEmis');
+		//Valores Contenidos en los TAGS
+            $fl=$folio->item(0)->nodeValue;
+            $fh=$fecha->item(0)->nodeValue;
+            $tp=$TipoDTE->item(0)->nodeValue;
+		}
+			
+        $emisors=$doc->getElementsByTagName('Emisor');
+		foreach($emisors as $emisor)
+		{
+	//Acceso al TAG 
+			$rut=$emisor->getElementsByTagName('RUTEmisor');
+			$razon=$emisor->getElementsByTagName('RznSoc');
+			$giro=$emisor->getElementsByTagName('GiroEmis');
+			$direccion=$emisor->getElementsByTagName('DirOrigen');
+			$comuna=$emisor->getElementsByTagName('CmnaOrigen');
+			$ciudad=$emisor->getElementsByTagName('CiudadOrigen');
+		//Valores Contenidos en los TAGS
+			@$re=$rut->item(0)->nodeValue;
+			@$rze=$razon->item(0)->nodeValue;
+			@$gre=$giro->item(0)->nodeValue;
+			@$dre=$direccion->item(0)->nodeValue;
+			@$coe=$comuna->item(0)->nodeValue;
+			@$cie=$ciudad->item(0)->nodeValue;
+        }
+        $totales=$doc->getElementsByTagName('Totales');
+        foreach ($totales as $total) 
+        {
+        //Acceso al TAG		
+            $definitivo=$total->getElementsByTagName('MntTotal');
+            $iva=$total->getElementsByTagName('IVA');
+            if(!$exento=$total->getElementsByTagName('MntExe'))
+            {
+                @$ex=$exento->item(0)->nodeValue;
+            }else{
+                $ex=0;
+            }
+            $neto=$total->getElementsByTagName('MntNeto');
+        //Valores Contenidos en los TAGS
+            @$df=$definitivo->item(0)->nodeValue;
+            @$iv=$iva->item(0)->nodeValue;
+            @$nt=$neto->item(0)->nodeValue;
+        }
+
+        $sel=mysql_query("SELECT * FROM factura_recibida WHERE folio = $fl AND rut_emisor = '$re'");
+        $result=mysql_num_rows($sel);
+        
+        if($result == 0){
+            $sql=mysql_query("INSERT INTO `latinsyc_giasys`.`factura_recibida` (`id`, `folio`, `tipo`, `fechaemision`, `rut_emisor`, `total`, `iva`, `exento`, `neto`) VALUES (NULL, '$fl', '$tp', '$fh','$re','$df','$iv','$ex','$nt')");
+            $sql2=mysql_query("INSERT INTO `latinsyc_giasys`.`acreedor` (`rut_acreedor`, `razonsocial`, `giro`, `comuna`, `ciudad`, `direccion`) VALUES('$re','$rze','$gre','$coe','$cie','$dre')");
+            $detalles=$doc->getElementsByTagName('Detalle');
+            $i=0;
+            $nom='';
+            $des='';
+            foreach ($detalles as $detalle ) 
+            {
+                if(@$nombre[$i]=$detalle->getElementsByTagName('NmbItem')){
+                    @$nom=$nombre[$i]->item(0)->nodeValue;
+                }	
+                if(@$descripcion[$i]=$detalle->getElementsByTagName('DscItem')){
+                    @$des=$descripcion[$i]->item(0)->nodeValue;
+                }	
+                $cantidad[$i]=$detalle->getElementsByTagName('QtyItem');
+                $precio[$i]=$detalle->getElementsByTagName('PrcItem');
+                $unidad[$i]=$detalle->getElementsByTagName('UnmdItem');
+                $monto[$i]=$detalle->getElementsByTagName('MontoItem');
+                @$can=$cantidad[$i]->item(0)->nodeValue;
+                @$pr=$precio[$i]->item(0)->nodeValue;
+                @$un=$unidad[$i]->item(0)->nodeValue;
+                @$mon=$monto[$i]->item(0)->nodeValue;
+                $i++;
+                $sql3=mysql_query("INSERT INTO `latinsyc_giasys`.`factura_recibida_concepto` (`id`, `folio`, `rut_emisor`, `descripcion`, `nombre`, `cantidad`, `precio`, `unidad`, `monto`) 
+                VALUES (NULL, '$fl', '$re', '$des', '$nom', '$can', '$pr', '$un', '$mon');");
+            }
+
+        }else{
+            return false;
+        }    
+        return true;
+    }
+}
+
+
+?>
--- USERS/API/recepcion.php
+++ USERS/API/recepcion.php
@@ -1,14 +1,16 @@
 <?php 
-include("nusoap/src/nusoap.php");
+date_default_timezone_set('America/Santiago');
+include_once ("nusoap/src/nusoap.php");
+include_once ('leefactura.php');
 $emisorRut="76254347-8";
 $usuarioWs="24675367-9";
 $claveWs="MtVZCHZ8d351";
-//$fecha=date('Y-m-d');
-$fecha='2018-10-29';
+$fecha=date('Y-m-d');
 $objClienteSOAP = new soapclient('http://wstest.webfactura.net/wsWF.php?wsdl');
 $token = $objClienteSOAP->getToken($emisorRut, $usuarioWs, $claveWs);
 $recibidos = $objClienteSOAP->getComprasJson($token, $emisorRut, $fecha);
-
+$res=0;
+$coun=0;
 $arrayRec =json_decode($recibidos, true);
 foreach ($arrayRec as $key){
     $folio=$key['dteFolio'];
@@ -18,10 +20,21 @@ foreach ($arrayRec as $key){
     $dteXml=$key['dteXml'];
     $xmldec=base64_decode($dteXml);
 
-    $fp = fopen('Logs/Recepcion/'.$folio.'_'.$emite.'_'.$dteTipo_id.'.xml', 'w');
+    $xml='Logs/Recepcion/'.$folio.'_'.$emite.'_'.$dteTipo_id.'_'.$emision.'.xml';
+    $fp = fopen($xml, 'w');
     fwrite($fp, $xmldec);
     fclose($fp);
 
+    $lector = new leeXml();
+    $reg=$lector->getDtes($xml);
+    if($reg){
+        $coun++;
+    }else{
+        $res++;
+    }
+
 }
+echo 'Exitosos '.$coun.'<br>';
+echo 'Fallidos '.$res;
 
 ?>
