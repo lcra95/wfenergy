8d7bce0b01dcf2c991fca151eb9ef806aa58a619
Elianny <admin@lrsystems.cl>
2018-12-09 21:07:38 -0300

    exporta pagos a excel

--- USERS/ADMON/export_pagos.php
+++ USERS/ADMON/export_pagos.php
@@ -1,3 +1,9 @@
+<?PHP 
+    header('Content-Type: application/vnd.ms-excel');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('content-disposition: attachment;filename=proceso_de_pago.xls');
+?>}
 <html>
 <head>
 <meta http-equiv=”Content-Type” content=”text/html; charset=utf-8″ />
@@ -50,11 +56,6 @@ $rut=explode('-', $q['rut_emisor']);
 </table>
  </body>
 </html>
-<?PHP 
-    header('Content-Type: application/vnd.ms-excel');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    header('content-disposition: attachment;filename=Pendiente_por_Facturar.xls');
-?>
+
 
 
